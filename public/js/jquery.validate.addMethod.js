05619d5bbe5abaabb46b33fb250acf291f9efa94
fabiomacedomendes <fabiomacedomendes@gmail.com>
2018-01-09 16:46:26 -0200

    remove external deps

--- public/js/jquery.validate.addMethod.js
+++
@@ -1,8 +0,0 @@
-/*
-* Adiciona metodo para validar se textarea com TinyMice contem valor
-*/
-jQuery.validator.addMethod("requiredTinyMice", function(value, element) {
-
-    length = tinymce.trim(tinymce.activeEditor.getBody().innerText).length;
-    return  (length > 0);
-}, "");
